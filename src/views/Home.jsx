7d474c3bce6622bf6cf8886a094e9c2fa42ac03c
Patricia <patricia@netcraft.com>
2026-03-09 13:17:18 +0000

    added file for miriam

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
   return (
     <Box sx={{ py: 4 }}>
       <Typography variant="h3" component="h1" gutterBottom>
-        Your career finally has a sage space
+        Your career <Box component="span" sx={{ fontStyle: 'italic' }}>finally</Box> has a safe space
       </Typography>
       <Typography variant="body1" color="text.secondary">
         Welcome to our community. Explore profiles, check out events, and read our guidelines to get started.
